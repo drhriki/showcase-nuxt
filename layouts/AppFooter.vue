07cfde202bab45646299a84ae24169fec1b73eb3
Riki.hermawan <riki.hermawan@ams.co.id>
2023-11-02 17:04:19 +0700

    first commit

---
+++ layouts/AppFooter.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { useLayout } from './composables/layout';
+import { computed } from 'vue';
+const { layoutConfig } = useLayout();
+const logoUrl = computed(() => {
+    return `/layout/images/${layoutConfig.darkTheme.value ? 'ams' : 'ams'}.png`;
+});
+</script>
+
+<template>
+    <div class="layout-footer">
+        <img :src="logoUrl" alt="Logo" height="20" class="mr-2" />
+        <span class="font-medium">© 2023 | PT Antarmitra Sembada</span>
+    </div>
+</template>
+<style lang="scss" scoped></style>
